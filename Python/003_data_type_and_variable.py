0126761705e43a6f2c1c3000d8522c115a1f45b6
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-10-31 23:22:47 +0800

    data and variable

---
+++ Python/003_data_type_and_variable.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+#print value of these variable
+n=123
+f=456.789
+s1='Hello, world'
+s2='Hello, \'Adam\''
+s3=r'Hello,"Bart"'
+s4=r'''Hello,
+Lisa!'''
+print(n,f,s1,s2,s3,s4)
