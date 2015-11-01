a52f9beb8a7bf251a2f96e9ad4af6561cb586c5a
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-01 22:50:01 +0800

    unfinished

---
+++ JavaScript/004_array.js
@@ -0,0 +1,9 @@
+'use strict'
+//arr.length
+//arr[]
+//arr.indexOf();   return index
+//arr.slice(0,3)   include 0,1,2 
+//arr.slice(3)   from 3 to the end
+//arr.slice()   a copy of arr
+//arr.push()   add elements to the end 
+//arr.pop()    delete the last element
