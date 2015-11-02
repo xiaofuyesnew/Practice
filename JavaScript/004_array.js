33fc3572d3504b9eeaef562dda3ce6bfbf07ab86
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-02 13:06:43 +0800

    004_array

--- JavaScript/004_array.js
+++ JavaScript/004_array.js
@@ -1,9 +1,23 @@
 'use strict'
-//arr.length
-//arr[]
+//arr.length;
+//arr[];
 //arr.indexOf();   return index
-//arr.slice(0,3)   include 0,1,2 
-//arr.slice(3)   from 3 to the end
-//arr.slice()   a copy of arr
-//arr.push()   add elements to the end 
-//arr.pop()    delete the last element
+//arr.slice(0,3);   include 0,1,2 
+//arr.slice(3);   from 3 to the end
+//arr.slice();   a copy of arr
+//arr.push();   add elements to the end 
+//arr.pop();    delete the last element
+//arr.unshift();    add element in the front
+//arr.shift();   delete first element from the front
+//arr.sort();    in order of element
+//arr.reverse();   up side down 
+//arr.splice(2,3,new1,new2);  delete from index 2 count 3 ,add new1, new2
+//arr1.concat(arr2);    connect arr1 and arr2
+//arr.join('-');     connect elements of arr with '-'
+//how to get the 500?
+var arr=[[1,2,3],[400,500,600],'-'];
+var x=arr[1][1];
+alert(x);
+//welcome A B C D
+var arr1=['A','B','C','D'];
+alert('welcome, '+arr1[0]+', '+arr1[1]+', '+arr1[2]+' and '+arr1[3]+'!');
