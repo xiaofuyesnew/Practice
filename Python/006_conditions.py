bcf21c9cf7d7a3968dfa4bbca9bd16bab34501d2
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-14 23:48:08 +0800

    update 2015-11-14

--- Python/006_conditions.py
+++ Python/006_conditions.py
@@ -19,3 +19,16 @@ ignore the other
 input data type is string
 
 '''
+height=1.75
+weight=80.5
+bmi=weight/height**2
+if bmi<18.5:
+	print("too light")
+elif bmi<25:
+	print("normal")
+elif bmi<28:
+	print("over weight")
+elif bmi<32:
+	print("fat")
+else:
+	print("too fat")
---
+++ Python/007_loop.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+'''
+range(5)
+[0,1,2,3,4]
+
+'''
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+	print("Hello, "+name+"!")
