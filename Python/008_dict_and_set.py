75dd04b553f390912d68395c6e325b4893d3ef1d
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-15 22:48:46 +0800

    update 2015-11-15 22:48

--- Python/008_dict_and_set.py
+++ Python/008_dict_and_set.py
@@ -11,6 +11,21 @@ default return None
 delete a key 
 dict.pop('key')
 
-set only key have NO repetition
+set only key and have NO repetition
+there needs a list to enter key 
+s=set([1,2,3])
+s
+{1,2,3}
+add a key to set 
+add(key) method
+set.add(key)
+delete a key from set
+remove(key) method
+set.remove(key)
+
+s1=set([1,2,3])
+s2=set([2,3,4])
+s1&s2     {2,3}
+s1|s2     {1,2,3,4}
 
 '''
