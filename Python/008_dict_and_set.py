8203b57fad485a27170ae9f6f2f59556ab10c0f7
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-15 22:35:56 +0800

    update

---
+++ Python/008_dict_and_set.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+'''
+dict={'key':value,}
+judge a key in dict or not use 'in'
+'key' in dict
+ture or false
+get  method
+dict.get('key',return value)
+default return None
+delete a key 
+dict.pop('key')
+
+set only key have NO repetition
+
+'''
