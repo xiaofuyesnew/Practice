cb0bcb317c9334b9ba23af4673677d1283285f51
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-23 21:33:06 +0800

    update 2015-11-23 21:33

--- JavaScript/007_map_and_set.js
+++ JavaScript/007_map_and_set.js
@@ -3,3 +3,16 @@
 var m=new Map();
 var s=new Set();
 alert('your browser support map and set')
+/*
+Map can fast find
+var m=new Map([['A',1],['B',2],['C',3]]);
+m.get('A');
+m.set('D',4); new key-value
+m.has('A');
+m.delete('A');
+
+Set has only key
+var s1=new Set();
+s1.add(new_key);
+s1.delete(key);
+*/
