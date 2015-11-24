e593b13339310663959f5d2a81a294549789717c
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-24 17:01:27 +0800

    update 2015-11-24 17:01

---
+++ JavaScript/008_iterable.js
@@ -0,0 +1,13 @@
+'use strict';
+/*
+iterable 
+array  -- subscript
+Map and Set ---   for ... of(ES6)
+
+*/
+var a=[1,2,3];
+for (var x of a){
+	alert(x);
+}
+alert('your browser can support  for...of');
+//for..in include some other properties
