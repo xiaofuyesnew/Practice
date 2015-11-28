d4173f7e3be22be2b8c49ca9ebe5c685de2f66d3
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-28 23:17:00 +0800

    update 2015-11-28 23:16

--- JavaScript/008_iterable.js
+++ JavaScript/008_iterable.js
@@ -11,3 +11,30 @@ for (var x of a){
 }
 alert('your browser can support  for...of');
 //for..in include some other properties
+//better method 'forEach' method
+var ex=['a','b','c'];
+ex.forEach(
+	function(element,index,array) {
+		alert(element);
+	}
+);
+
+/*
+	Set has no index
+	var s=new Set(['a','b','c']);
+	s.forEach(
+		function(element,sameElement,set){
+		
+		}
+	);
+	
+	for Map
+	var m=new Map([[1,'x'],[2,'y'],[3,'z']]);
+	m.forEach(
+		function(value,key,map){
+			
+		}
+	);
+	
+*/ 
+
