f8706af40d5a6df75daf36dc041dc6d5b1478db2
xiaofuyesnew <xiaofuyesnew@qq.com>
2015-11-28 23:50:27 +0800

    update 2015-11-28 23:50

---
+++ JavaScript/009_function.js
@@ -0,0 +1,16 @@
+'use strict';
+//define and invoking function
+/**
+ *     function name(x){
+ *     		some sentences;
+ *          return ?;
+ *     }
+ *      
+ *     var name=function(x){
+ * 			soem centences;
+ * 			return ?;
+ *     };
+ * 
+ * 
+ * 
+ */
